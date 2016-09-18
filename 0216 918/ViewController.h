bec52f73a4717cebee656519ac0af403330edae7
DC016 <dc016@mayun.lan>
2016-09-18 09:52:07 +0800

    Initial Commit

---
+++ 0216 918/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0216 918
+//
+//  Created by DC016 on 16/9/18.
+//  Copyright © 2016年 马运. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 0216 918/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  0216 918
+//
+//  Created by DC016 on 16/9/18.
+//  Copyright © 2016年 马运. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
